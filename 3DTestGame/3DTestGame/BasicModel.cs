9fe83fd12bfede4336ef605eac2f217ab10d1280
Roy McElmurry <shakalandro@gmail.com>
2012-04-11 11:16:52 -0700

    added Models folder

---
+++ 3DTestGame/3DTestGame/BasicModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _3DTestGame
+{
+    class BasicModel
+    {
+        public Model model {
+            get;
+            protected set;
+        }
+        protected Matrix world = Matrix.Identity;
+
+        public BasicModel(Model m)
+        {
+            this.model = m;
+        }
+        public virtual void update()
+        {
+
+        }
+        public void draw(Camera camera)
+        {
+            
+        }
+    }
+}
