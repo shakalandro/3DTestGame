24795dcefafd66fdeb9ff0c936dace6f040f096e
Sean <skimforever@gmail.com>
2012-05-15 01:17:27 -0700

    Transfered over to BEPU physics engine, have collisions between moving model and terrain

--- 3DTestGame/3DTestGame/Camera.cs
+++ 3DTestGame/3DTestGame/Camera.cs
@@ -40,7 +40,7 @@ namespace _3DTestGame
             this.up = up;
             float aspectRatio = Game.Window.ClientBounds.Width / Game.Window.ClientBounds.Height;
             this.projection = Matrix.CreatePerspectiveFieldOfView(
-                MathHelper.PiOver4, aspectRatio, 1, 100);
+                MathHelper.PiOver4, aspectRatio, 1, 300);
             CreateLookAt();
             this.input = ((ISTestGame)game).input;
         }
---
+++ 3DTestGame/3DTestGame/ModelClasses/StaticModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _3DTestGame
+{
+    /// <summary>
+    /// Component that draws a model.
+    /// </summary>
+    public class StaticModel : DrawableGameComponent
+    {
+        Model model;
+        /// <summary>
+        /// Base transformation to apply to the model.
+        /// </summary>
+        public Matrix Transform;
+        Matrix[] boneTransforms;
+
+
+        public ICamera camera
+        {
+            get
+            {
+                return (ICamera)this.Game.Services.GetService(typeof(ICamera));
+            }
+        }
+
+        /// <summary>
+        /// Creates a new StaticModel.
+        /// </summary>
+        /// <param name="model">Graphical representation to use for the entity.</param>
+        /// <param name="transform">Base transformation to apply to the model before moving to the entity.</param>
+        /// <param name="game">Game to which this component will belong.</param>
+        public StaticModel(Model model, Matrix transform, Game game)
+            : base(game)
+        {
+            this.model = model;
+            this.Transform = transform;
+
+            //Collect any bone transformations in the model itself.
+            //The default cube model doesn't have any, but this allows the StaticModel to work with more complicated shapes.
+            boneTransforms = new Matrix[model.Bones.Count];
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            model.CopyAbsoluteBoneTransformsTo(boneTransforms);
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = boneTransforms[mesh.ParentBone.Index] * Transform;
+                    effect.View = camera.view;
+                    effect.Projection = camera.projection;
+                }
+                mesh.Draw();
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
